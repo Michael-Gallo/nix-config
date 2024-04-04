793d6b6fcf4e3058bac54c28576c378aa5d6f455
Michael Gallo <michaelgallo@protonmail.com>
2024-04-04 17:02:33 +0200

    enable teamviewer service

--- configuration.nix
+++ configuration.nix
@@ -17,17 +17,20 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  services.teamviewer.enable = true;
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = null;
+
   # Try to use localtime
   services.localtimed.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "America/New_York";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
