1ce27938ecf1e456f97dd7f5aa0116e9c38b0c57
Michael Gallo <michaelgallo@protonmail.com>
2024-04-04 11:22:52 +0200

    add tldr

--- home.nix
+++ home.nix
@@ -75,6 +75,7 @@
     joplin-desktop
     xclip
     firefox
+    tldr
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
