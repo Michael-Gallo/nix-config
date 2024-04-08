bb34f9b988322063fdbea35789b554aa902bbe96
Michael Gallo <michaelgallo@protonmail.com>
2024-04-08 16:10:13 +0200

    add power config to config.nix

--- configuration.nix
+++ configuration.nix
@@ -53,7 +53,9 @@
   # Enable the Cinnamon Desktop Environment.
   services.xserver.displayManager.lightdm.enable = true;
   services.xserver.desktopManager.cinnamon.enable = true;
-
+  # virtualbox settings
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "mike" ];
   # Configure keymap in X11
   services.xserver = {
     layout = "us";
@@ -147,6 +149,31 @@ programs.zsh.enable = true;
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
 
+  # Laptop power settings (move to laptop.nix later)
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+  };
+  services.tlp = {
+      enable = true;
+      settings = {
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+        CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+
+        CPU_MIN_PERF_ON_AC = 0;
+        CPU_MAX_PERF_ON_AC = 100;
+        CPU_MIN_PERF_ON_BAT = 0;
+        CPU_MAX_PERF_ON_BAT = 40;
+
+       #Optional helps save long term battery health
+       START_CHARGE_THRESH_BAT0 = 75; # 40 and bellow it starts to charge
+       STOP_CHARGE_THRESH_BAT0 = 90; # 80 and above it stops charging
+
+      };
+};
 
 
 }
