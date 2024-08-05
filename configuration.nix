89817c82f90ae6332baaefe67cab653126bd8776
Michael Gallo <michaelgallo@protonmail.com>
2024-08-04 22:04:48 -0400

    remove annoying bell

--- configuration.nix
+++ configuration.nix
@@ -79,6 +79,14 @@
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
     #media-session.enable = true;
+    # Get rid of the insanely annoying bell
+    extraConfig.pipewire = {
+     "99-disable-bell" = {
+       "context.properties"= {
+            "module.x11.bell" = false;
+       };
+     };
+    };
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -105,7 +113,6 @@ programs.zsh.enable = true;
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
-
   nix = {
 	# package = pkgs.nixUnstable;
 	extraOptions = ''
@@ -147,7 +154,6 @@ programs.zsh.enable = true;
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
-
   programs.kdeconnect.enable = true;
   # Laptop power settings (move to laptop.nix later)
   powerManagement = {
