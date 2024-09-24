d029a9f45b8da62b908a8840fda1fa07e64c7bc3
Michael Gallo <michaelgallo@protonmail.com>
2024-09-24 09:27:37 -0400

    hopefull ybetter power settings

--- configuration.nix
+++ configuration.nix
@@ -176,6 +176,8 @@ programs.zsh.enable = true;
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "23.11"; # Did you read the comment?
   programs.kdeconnect.enable = true;
+  # Fingerprint (possible move to laptop.nix)
+  services.fprintd.enable = true;
   # Laptop power settings (move to laptop.nix later)
   powerManagement = {
     enable = true;
@@ -185,19 +187,19 @@ programs.zsh.enable = true;
       enable = true;
       settings = {
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 100;
+        CPU_MAX_PERF_ON_BAT = 60;
 
        #Optional helps save long term battery health
-       START_CHARGE_THRESH_BAT0 = 75; # 40 and bellow it starts to charge
-       STOP_CHARGE_THRESH_BAT0 = 90; # 80 and above it stops charging
+       START_CHARGE_THRESH_BAT0 = 50; # 40 and bellow it starts to charge
+       STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
 
       };
 };
